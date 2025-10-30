80f03f0aab2888dcc218eb400c61c0120bf952b4
Ivan Neutov <ivan.neutov@icloud.com>
2025-10-31 00:18:27 +0300

    add timestamp

--- gpt2giga/logger.py
+++ gpt2giga/logger.py
@@ -11,7 +11,8 @@ def init_logger(log_level: str) -> logging.Logger:
     if not logger.handlers:
         handler = logging.StreamHandler(sys.stdout)
         formatter = logging.Formatter(
-            fmt="%(asctime)s [%(levelname)s] %(name)s: %(message)s", datefmt="%H:%M:%S"
+            fmt="%(asctime)s.%(msecs)03d [%(levelname)s] %(name)s: %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
         )
         handler.setFormatter(formatter)
         logger.addHandler(handler)
